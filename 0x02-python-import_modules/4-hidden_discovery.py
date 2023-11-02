78c13ca21b1d873298e21d94d3716c9c7e93f51f
KipruttoMutai <mutaikiprutto@gmail.com>
2023-11-02 06:43:03 -0700

    a program that prints all the names defined by the compiled module hidden_4.pyc

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import  hidden_4
+    name = dir( hidden_4)
+    for names in name:
+        if not names.startswith('_'):
+            print("{}".format(names))
