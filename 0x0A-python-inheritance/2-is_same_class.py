39f134c2aca9e0d19e05c4f31777914056741d3a
KipruttoMutai <mutaikiprutto@gmail.com>
2023-12-04 05:48:16 -0800

    Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A function that checks instance of class"""
+
+
+def is_same_class(obj, a_class):
+    """ Returns True if object is instance of class and Flase otherwise"""
+    return isinstance(obj, a_class)
