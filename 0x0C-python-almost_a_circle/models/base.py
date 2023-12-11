1e4fe693eb3293403a3d87ab7b7f3b52d12d9dab
KipruttoMutai <mutaikiprutto@gmail.com>
2023-12-11 15:35:49 -0800

    Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,9 +5,11 @@
 class Base:
     """Represents a Base model"""
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """initialization"""
+
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects += 1
             self.id = Base.__nb__objects
