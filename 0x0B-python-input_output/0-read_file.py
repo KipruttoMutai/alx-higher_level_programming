e8064ba753c04c1aaea4ac295c4b08456a10c884
KipruttoMutai <mutaibrian04@gmail.com>
2024-02-06 08:21:55 -0800

    A function to open, read and print file to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 def read_file(filename=""):
     """Opens file and reads it"""
-    with open(filename, 'r', encoding='utf-8') as txt:
+    with open(filename, encoding='utf-8') as txt:
         for line in txt:
             print(line, end='')
