5faf80b3adfb816d0a037257f6637ae3d6f9a45e
KipruttoMutai <mutaibrian04@gmail.com>
2024-02-06 08:25:20 -0800

    reads a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
+"""
+module: 0-read_file
+resources: read_file function
+"""
+
 
 def read_file(filename=""):
-    """Opens file and reads it"""
-    with open(filename, encoding='utf-8') as txt:
-        for line in txt:
+    """Opens file and reads it
+    and prints it to stdout
+    """
+    with open(filename, encoding='utf-8') as file:
+        for line in file:
             print(line, end='')
