b67d4dca12092dde508402dc9b55ae594709a454
KipruttoMutai <mutaibrian04@gmail.com>
2024-02-06 08:13:00 -0800

    #!/usr/bin/python3

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 def read_file(filename=""):
     """Opens file and reads it"""
-    with open(filename, 'r') as txt:
+    with open(filename, 'r', encoding='utf-8') as txt:
         txt = txt.read()
         print(txt, end="")
