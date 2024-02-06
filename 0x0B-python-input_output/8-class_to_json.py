642d752f88a6467a44274d107616e0dad2285992
KipruttoMutai <mutaibrian04@gmail.com>
2024-02-06 12:29:33 -0800

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-"""function to convert class to json"""
+"""
+module:8-class_to_json
+resource:class_to_json()
+"""
 
 
 def class_to_json(obj):
@@ -8,10 +11,4 @@ def class_to_json(obj):
     returns the dictionary description with simple data structure
     for JSON serialization of an object
     """
-    if not hasattr(obj, '__dict__'):
-        raise TypeError("Object is not an instance of a class")
-    json_dict = {}
-    for attr_name, attr_value in obj.__dict__.items():
-        if isinstance(attr_value, (list, dict, str, int, bool)):
-        json_dict[attr_name] = attr_value
-    return json_dict
+    return obj.__dict__
