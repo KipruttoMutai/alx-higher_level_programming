1c2796bae83fad80f010f1f87f288f6a750455d0
KipruttoMutai <mutaibrian04@gmail.com>
2024-02-07 00:26:27 -0800

    Compare 2 squares

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+"""Define a class square"""
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size=0):
+        """Initializes square
+        Args:
+            size (int): The size of the new square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieves a private attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Sets a new value to the attribute size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns area of square
+           Args:
+               self: the instance
+        """
+        return self.__size * self.__size
+
+    def __eq__(self, other):
+        """Define the == comparision to a Square."""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """Define the != comparison to a Square."""
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        """Define the < comparison to a Square."""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """Define the <= comparison to a Square."""
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        """Define the > comparison to a Square."""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """Define the >= compmarison to a Square."""
+        return self.area() >= other.area()
