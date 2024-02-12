cba7f7dd2e6a657fd18967e742b129882cff39af
KipruttoMutai <mutaibrian04@gmail.com>
2024-02-12 14:03:14 -0800

    And now, the Square!

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,8 +10,8 @@ class Square(Rectangle):
     """creates a square"""
     def __init__(self, size, x=0, y=0, id=None):
         """initializes square with its attributes inherited from rectangle"""
-        super().__init__(size, size, x, y, id)
+        super().__init__(height=size, width=size, x=x, y=y, id=id)
 
     def __str__(self):
         """returns [Square] (<id>) <x>/<y> - <size>"""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.height}"
