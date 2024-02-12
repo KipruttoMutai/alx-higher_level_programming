f1dc1260b2ea47a7feab146c271a471de40c0e86
KipruttoMutai <mutaibrian04@gmail.com>
2024-02-11 23:18:36 -0800

    Base Class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+modules:base.py
+resources:class base
+"""
+
+
+class Base:
+    """creates a base"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+         assigns the public instance attribute id with this argument value
+         if id is not none otherwise  assigns the new value to
+         the public instance attribute id
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
